c938dc37f173bfe4cc264180cd54596b12cc99d2
BinhAnndapoet <dinhlebinhan2005@gmail.com>
2026-03-28 20:14:18 +0700

    update config/gemini.py

--- config/gemini.py
+++ config/gemini.py
@@ -7,10 +7,9 @@ def get_llm(model_name: str = "gemini-2.5-flash", temperature: float = 0):
         model=model_name,
         temperature=temperature,
         api_key=GEMINI_API_KEY,
-
     )
 
-def get_embeddings(model_name: str = "models/text-embedding-004"):
+def get_embeddings(model_name: str = "models/gemini-embedding-001"):
     return GoogleGenerativeAIEmbeddings(
         model=model_name,
         google_api_key=GEMINI_API_KEY
